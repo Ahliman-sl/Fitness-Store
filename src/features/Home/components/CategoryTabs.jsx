a07c81031b70d8474d21ba7450cb2cec3a789839
Ahliman-sl <ahliman.slymn@gmail.com>
2024-07-06 02:59:49 +0400

    fixed v1

--- src/features/Home/components/CategoryTabs.jsx
+++ src/features/Home/components/CategoryTabs.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
 import { FaArrowAltCircleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 function CategoryTabs({ icon, title, text }) {
   return (
     <div className="w-[20rem] h-max p-5 bg-[#111111]  flex flex-col gap-2 items-start justify-center">
       {icon}
       <h2 className="text-2xl text-slate-50 font-bold ">{title}</h2>
       <p className="text-slate-200">{text}</p>
-      <div className="w-full flex justify-end">
+      <Link className="w-full flex justify-end" to="/articles">
         <FaArrowAltCircleRight className="text-slate-50 text-4xl hover:scale-[1.1] hover:text-rose-500 cursor-pointer transition duration-300" />
-      </div>
+      </Link>
     </div>
   );
 }
